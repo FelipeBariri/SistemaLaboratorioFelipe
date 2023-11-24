f9cc71d42aeea7fdd0731a4b5f6d380d7ecf5855
FelipeBariri <ephzip@gmail.com>
2023-11-24 08:32:14 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaLaboratorio.Models
+{
+    public class Contexto
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+
+        }
+        //public DbSet<Calculadora> Calculadora { get; set; }
+    }
+}
